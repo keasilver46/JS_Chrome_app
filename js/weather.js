4b17c9e3be75a0228baf3482ef5deae859fb6788
keasilver46 <keasilver46@gmail.com>
2022-08-05 23:35:26 +0900

    CSS 수정

--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,10 @@ function onGeoOk(position) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      const temp = document.querySelector("#weather div:first-child");
+      const city = document.querySelector("#weather div:last-child");
+      city.innerText = `${data.name}`;
+      temp.innerText = `${data.main.temp}ºC`;
     });
 }
 
